0a40c1b69ef4722b1cf347124860b9b7d9372b4b
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2023-10-07 11:42:08 +0900

    feat: 멤버 엔티티 생성 (#5)

--- src/main/java/com/kukertone/kukertone_be/KukertoneBeApplication.java
+++ src/main/java/com/kukertone/kukertone_be/KukertoneBeApplication.java
@@ -1,9 +1,12 @@
 package com.kukertone.kukertone_be;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://yeongjin.site")})
 @SpringBootApplication
 public class KukertoneBeApplication {
 
---
+++ src/main/java/com/kukertone/kukertone_be/domain/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.kukertone.kukertone_be.domain.member.controller;
+
+public class MemberController {
+}
