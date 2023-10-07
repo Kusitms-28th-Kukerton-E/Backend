1ca0e6709403f4df1e0247d2ef9cdeb6f339dd97
nohy6630 <nohy6630@gmail.com>
2023-10-07 18:25:35 +0900

    fix: 오타 수정

--- src/main/java/com/kukertone/kukertone_be/domain/member/entity/Member.java
+++ src/main/java/com/kukertone/kukertone_be/domain/member/entity/Member.java
@@ -1,13 +1,8 @@
 package com.kukertone.kukertone_be.domain.member.entity;
 
+import jakarta.persistence.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
