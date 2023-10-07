566c0f608db41345b020ca7ae7f14e90417d5eba
nohy6630 <nohy6630@gmail.com>
2023-10-07 21:58:00 +0900

    feat: 게시글 카테고리 추가

--- src/main/java/com/kukertone/kukertone_be/domain/article/controller/ArticleController.java
+++ src/main/java/com/kukertone/kukertone_be/domain/article/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.kukertone.kukertone_be.domain.article.controller;
 
+import com.kukertone.kukertone_be.domain.article.Category;
 import com.kukertone.kukertone_be.domain.article.dto.request.ArticleRequest;
 import com.kukertone.kukertone_be.domain.article.dto.response.ArticleListResponse;
 import com.kukertone.kukertone_be.domain.article.dto.response.ArticleResponse;
@@ -19,6 +20,11 @@ public class ArticleController{
     @GetMapping()
     public ArticleListResponse getAll(){
         return articleService.getAll();
+    }
+
+    @GetMapping("/{category}")
+    public ArticleListResponse getAll(@PathVariable Category category){
+        return articleService.getAllByCategory(category);
     }
 
     @GetMapping("/{articleId}")
