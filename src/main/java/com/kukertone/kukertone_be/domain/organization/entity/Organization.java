b8a29022dfef0e7e1bb7f87ed58e476076142467
nohy6630 <nohy6630@gmail.com>
2023-10-08 05:12:51 +0900

    fix: 오타 수정

--- src/main/java/com/kukertone/kukertone_be/domain/organization/entity/Organization.java
+++ src/main/java/com/kukertone/kukertone_be/domain/organization/entity/Organization.java
@@ -4,8 +4,6 @@ import com.kukertone.kukertone_be.domain.article.entity.Article;
 import com.kukertone.kukertone_be.domain.kid.entity.Kid;
 import com.kukertone.kukertone_be.domain.member.entity.Member;
 import com.kukertone.kukertone_be.domain.member.entity.Role;
-import com.kukertone.kukertone_be.domain.organizationReview.entity.OrganizationReview;
-import com.kukertone.kukertone_be.domain.volunteerReview1.entity.VolunteerReview1;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
