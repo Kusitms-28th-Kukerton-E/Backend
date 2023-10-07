b8aca2471041e5241c8938516362c649dfa153d1
nohy6630 <nohy6630@gmail.com>
2023-10-07 11:39:47 +0900

    feat: 멤버 엔티티 생성

---
+++ src/main/java/com/kukertone/kukertone_be/domain/member/entity/Member.java
@@ -0,0 +1,7 @@
+package com.kukertone.kukertone_be.domain.member.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Member {
+}
---
+++ src/main/java/com/kukertone/kukertone_be/domain/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.kukertone.kukertone_be.domain.member.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository {
+}
